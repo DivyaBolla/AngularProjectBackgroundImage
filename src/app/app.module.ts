e71ed70faedb3be1ac4e56130b41c174fe37c187
DivyaBolla <ramadivyabolla@gmail.com>
2023-07-12 17:21:01 -0400

    InitialCommit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { FirstcomponentComponent } from './components/firstcomponent/firstcomponent.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FirstcomponentComponent,
+  
   ],
   imports: [
     BrowserModule
